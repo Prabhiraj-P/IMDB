aff6db14fa9fa745ccc94c4b64ea50174d4be165
Deadcode87 <128582548+Deadcode87@users.noreply.github.com>
2023-09-06 18:22:39 +0530

    updated runtime function

--- lambda_function.py
+++ lambda_function.py
@@ -57,28 +57,17 @@ def release_info(id):
        return date.text
 
 
-def runtime(id):
-    key_url='https://www.imdb.com/title/tt'+id+'/technical/'
-    #https://www.imdb.com/title/tt'+id_+'/'
-    response_runtime=requests.get(key_url,headers=headers)
-    content_runtime=response_runtime.content
-    soup_runtime=BeautifulSoup(content_runtime,'html.parser')
-  
-    runtime=soup_runtime.find('li',class_='ipc-metadata-list__item').text
-    try:
-     color=soup_runtime.find_all('a',class_='ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link')[1].text
-    except:
-      color='NaN'
-    
-    if runtime is not []:
-     if 'Runtime' in runtime:
-      runtime=runtime.replace('Runtime','') 
-      #print('runtime')
-      return runtime ,color
-    else:
-      #print('runtime')
-      runtime
-    return 'NaN',color 
+def runtime(soup):
+ runtime_test=soup.find_all('li',class_='ipc-metadata-list__item')
+ color='NaN'
+ runtime="NaN"
+ for i in runtime_test:
+    if 'Runtime' in i.text:
+     runtime=i.text.replace('Runtime','')   
+    if 'Color' in i.text:
+      color=i.text #.replace('Color',' ')
+ return runtime,color
+      
     
 #Country fuction
 
@@ -96,74 +85,90 @@ def country(soup):
     #print('country')
     return country_name
 
+movie_genres = ["Action","Adventure","Comedy","Drama","Horror","Science Fiction (Sci-Fi)","Fantasy","Romance","Mystery","Crime","Thriller","Animation","Musical","War","Western","Biography","Historical",
+    "Documentary","Family","Sports","Superhero","Musical Comedy","Film Noir","Romantic Comedy (Rom-Com)","Fantasy Adventure","Action Comedy","Psychological Thriller","Science Fantasy",
+    "Epic","Post-Apocalyptic","History"]   
+
+def genere_fun(soup):
+ gen_list=' '
+ try:
+  genres = soup.find_all('span', class_='ipc-chip__text')
+  for g in  genres:
+    if g.text in movie_genres:
+        gen_list=g.text+' '+gen_list
+  return gen_list 
+ except:
+  return 'NaN'
+    
 
 
-df={'Id':[],'Link':[],'Title':[],'Storyline':[],'Director':[],'Date':[],'Runtime':[],'Rating':[],'Color':[],'Country':[],'keywords':[]}
+
+df={'Id':[],'Title':[],'Storyline':[],'Genres':[],'Director':[],'Date':[],'Runtime':[],'Rating':[],'Color':[],'Country':[],'keywords':[]}
 
 def lambda_handler(event, context):
     i=1
     start_time=time.time()
     for num in range(where,100000000):
-                             len_num=7-len(str(num))
-                             id_='0'*len_num+str(num)
-                             w_url='https://www.imdb.com/title/tt'+id_+'/'
-                             try:
-                               response=requests.get(w_url,headers=headers)
-                               content=response.content
-                               soup=BeautifulSoup(content,'html.parser')
-                               Title=soup.find('span',class_='sc-afe43def-1 fDTGTb').text
-                             except:
-                               print(i,w_url,'>> Error')
-                               continue
-                             print(i,w_url,'<< processing')
-                             Title=soup.find('span',class_='sc-afe43def-1 fDTGTb').text
-                             try:
-                              storyline=soup.find_all('div',class_='ipc-html-content-inner-div')[2].text
-                             except:
-                               storyline='NaN'
-                             Director=soup.find_all('a',class_='ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link')[0].text
-                             Date=release_info(id_)
+      len_num=7-len(str(num))
+      id_='0'*len_num+str(num)
+      w_url='https://www.imdb.com/title/tt'+id_+'/'
+      try:
+       response=requests.get(w_url,headers=headers)
+       content=response.content
+       soup=BeautifulSoup(content,'html.parser')
+       Title=soup.find('span',class_='sc-afe43def-1 fDTGTb').text
+      except:
+       print(i,w_url,'>> Error')
+       continue
+      print(i,w_url,'<< processing')
+      Title=soup.find('span',class_='sc-afe43def-1 fDTGTb').text
+      try:
+       storyline=soup.find_all('div',class_='ipc-html-content-inner-div')[2].text
+      except:
+       storyline='NaN'
+      Director=soup.find_all('a',class_='ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link')[0].text
+      Date=release_info(id_)
  
-                             Runtime_,colr=runtime(id_)
-                             try:
-                              Rating=(soup.find_all('span',class_='sc-bde20123-1 iZlgcd')[0]).text
-                             except:
-                               Rating="NaN"
-                             country_=country(soup)
-                             df['Country'].append(country_)
-                             key_words=keywords(str(id_))
-                             i=i+1
-                             df['Id'].append(id_)
-                             df['Link'].append(w_url)
-                             df['Title'].append(Title)
-                             df['Storyline'].append(storyline)
-                             df['Director'].append(Director)
-                             df['Date'].append(Date)
-                             df['Runtime'].append(Runtime_)
-                             df['Color'].append(colr)
-                             df['Rating'].append(Rating)
-                             df['keywords'].append(key_words)
-                             if i==100:
-                               try:
-                                main_df=pd.DataFrame(df)
-                                save_df=pd.concat([load_df, main_df], ignore_index=True)
-                                save_df.to_csv(path,index=False,sep=';',header=True)
-                                load_df = pd.read_csv(path,sep=';')
-                                print('>>',load_df.shape)
-                                df={'Id':[],'Link':[],'Title':[],'Storyline':[],'Director':[],'Date':[],'Runtime':[],'Rating':[],'Color':[],'Country':[],'keywords':[]}
-                                end_time=time.time()
-                                print('>>Time', (end_time-start_time)/100)
-                                start_time=time.time()
-                               except:
-                                 main_df=pd.DataFrame(df)
-                                 save_df=main_df
-                                 save_df.to_csv(path,index=False,sep=';',header=True)
-                                 load_df = pd.read_csv(path,sep=';')
-                                 print('>>',load_df.shape)
-                                 df={'Id':[],'Link':[],'Title':[],'Storyline':[],'Director':[],'Date':[],'Runtime':[],'Rating':[],'Color':[],'Country':[],'keywords':[]}
-                                 print('File not found Solved')
- 
-                               i=1
+      Runtime_,colr=runtime(soup)
+      try:
+        Rating=(soup.find_all('span',class_='sc-bde20123-1 iZlgcd')[0]).text
+      except:
+       Rating="NaN"
+      country_=country(soup)
+      df['Country'].append(country_)
+      key_words=keywords(str(id_))
+      i=i+1
+      df['Id'].append(id_)
+      df['Link'].append(w_url)
+      df['Title'].append(Title)
+      df['Storyline'].append(storyline)
+      df['Director'].append(Director)
+      df['Genres'].append(genere_fun(soup))
+      df['Date'].append(Date)
+      df['Runtime'].append(Runtime_)
+      df['Color'].append(colr)
+      df['Rating'].append(Rating)
+      df['keywords'].append(key_words)
+      if i==100:
+       try:
+        main_df=pd.DataFrame(df)
+        save_df=pd.concat([load_df, main_df], ignore_index=True)
+        save_df.to_csv(path,index=False,sep=';',header=True)
+        load_df = pd.read_csv(path,sep=';')
+        print('>>',load_df.shape)
+        df={'Id':[],'Link':[],'Title':[],'Storyline':[],'Director':[],'Date':[],'Runtime':[],'Rating':[],'Color':[],'Country':[],'keywords':[]}
+        end_time=time.time()
+        print('>>Time', (end_time-start_time)/100)
+        start_time=time.time()
+       except:
+          main_df=pd.DataFrame(df)
+          save_df=main_df
+          save_df.to_csv(path,index=False,sep=';',header=True)
+          load_df = pd.read_csv(path,sep=';')
+          print('>>',load_df.shape)
+          df={'Id':[],'Link':[],'Title':[],'Storyline':[],'Director':[],'Date':[],'Runtime':[],'Rating':[],'Color':[],'Country':[],'keywords':[]}
+          print('File not found Solved')
+       i=1
     
     return {
         'statusCode': 200,
